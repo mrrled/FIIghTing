096d9503ec172b10b200c7b511856231d1e873e5
Алсу Исламова <alsu1slamova@yandex.ru>
2025-05-17 14:48:01 +0500

    add animation for buttons

--- Assets/Scripts/ButtonSound.cs
+++ Assets/Scripts/ButtonSound.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
     public AudioSource audioSource;
     public AudioSource audioClick;
-    
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         audioSource.PlayOneShot(audioSource.clip);
+        _animator.SetTrigger("Highlighted");
     }
-    
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _animator.SetTrigger("Normal");
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         audioClick.Play();
