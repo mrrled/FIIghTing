4eea7bae4407a4f92d51e495a66dbe99ad5e870f
MoskvinaAlexandra <Moskvina.Alexandra@urfu.me>
2025-05-19 01:49:48 +0500

    add stamina_bar & fix animations

--- Assets/Scripts/Hurtbox.cs
+++ Assets/Scripts/Hurtbox.cs
@@ -9,7 +9,7 @@ public class Hurtbox : MonoBehaviour
     public float maxHealth = 100f;
     public float currentHealth;
     public Image healthBar;
-    public Image staminaBar; //TODO: Прикрутить картинку Стамина Бара
+    public Image staminaBar;
     public float maxStamina = 100f;
     public float currentStamina;
     public AudioSource attackSound;
@@ -18,6 +18,10 @@ public class Hurtbox : MonoBehaviour
     {
         currentHealth = maxHealth;
         currentStamina = maxStamina;
+    }
+
+    private void FixedUpdate() {
+        staminaBar.fillAmount = currentStamina / maxStamina;
     }
 
     public void TakeDamage(float damage, Vector2 pushFrom)
@@ -38,11 +42,6 @@ public class Hurtbox : MonoBehaviour
         Debug.Log(gameObject.transform.parent.name + " заблокировал " + damage + " урона!");
         var blockedDamage = Math.Min(damage, currentStamina / 2);
         currentStamina = Math.Max(0f, currentStamina - 2 * damage);
-        //TODO: Убрать этот иф когда будет картинка стамина-бара
-        #if __DEBUG__
-        #else
-        staminaBar.fillAmount = currentStamina / maxStamina;
-        #endif
         return blockedDamage;
     }
 }
