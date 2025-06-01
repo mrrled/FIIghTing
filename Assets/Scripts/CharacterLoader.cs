b6feb26ba90ed39ed77182a75a2e3c9c436d1171
Алсу Исламова <alsu1slamova@yandex.ru>
2025-06-01 18:29:11 +0500

    fixes
    переделаны canvas на одно разрешение экрана

--- Assets/Scripts/CharacterLoader.cs
+++ Assets/Scripts/CharacterLoader.cs
@@ -13,10 +13,29 @@ public class CharacterLoader : MonoBehaviour
 
     private void LoadCharacter(int num)
     {
+        var camera = Camera.main;
+        var halfHeight = camera.orthographicSize;
+        var halfWidth = halfHeight * camera.aspect;
+        Debug.Log(num);
+        Debug.Log(halfWidth);
+        Debug.Log(halfHeight);
         var index = PlayerPrefs.GetInt($"SelectedCharacter{num}");
-        var character = num == 1 
-            ? characterDatabase1.GetCharacter(index) 
-            : characterDatabase2.GetCharacter(index);
-        Instantiate(character.characterPrefab);
+        var position = Vector3.zero;
+        position.y = camera.transform.position.y - halfHeight;
+        if (num == 1)
+        {
+            // position.x = camera.transform.position.x + halfWidth;
+            // position.x /= 2;
+            var character = characterDatabase1.GetCharacter(index);
+            Instantiate(character.characterPrefab, position, Quaternion.identity);
+        }
+        else
+        {
+            // position.x = camera.transform.position.x - halfWidth;
+            // position.x /= 2;
+            var character = characterDatabase2.GetCharacter(index);
+            Instantiate(character.characterPrefab, position, Quaternion.identity);
+        }
+        Debug.Log(position);
     }
 }
