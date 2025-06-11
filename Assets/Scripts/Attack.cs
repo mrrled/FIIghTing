9dc80315d4a87a806dd4f3cdd04939ccffa942e1
Алсу Исламова <alsu1slamova@yandex.ru>
2025-06-11 23:12:11 +0500

    update Attack.cs
    сделано намного проще, теперь хитбоксы появляются с анимацией

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -3,37 +3,11 @@ using UnityEngine.InputSystem;
 
 public class Attack : MonoBehaviour
 {
-    public Transform attackPoint;
     public Animator animator;
-    public GameObject existingHitboxObject;
     public string animationTrigger = "Attack";
-    public float attackDuration = 0.5f;
-    private bool _isAttacking;
-
-    void Start()
-    {
-        DeactivateHitbox();
-    }
 
     public void Attacking(InputAction.CallbackContext context)
     {
-        if (!context.performed || _isAttacking) return;
-        existingHitboxObject.transform.position = attackPoint.position;
-        existingHitboxObject.transform.rotation = attackPoint.rotation;
-        ActivateHitbox();
-        _isAttacking = true;
         animator.SetTrigger(animationTrigger);
-        Invoke(nameof(DeactivateHitbox), attackDuration);
-        _isAttacking = false;
-    }
-
-    void ActivateHitbox()
-    {
-        existingHitboxObject.SetActive(true);
-    }
-
-    void DeactivateHitbox()
-    {
-        existingHitboxObject.SetActive(false);
     }
 }
